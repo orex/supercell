b8a136a56f2ae2482c585bb38f9e5faf40e9375b
Kirill Okhotnikov <kirill.okhotnikov@gmail.com>
2015-06-04 10:26:13 +0200

    Exclude all output of examples.

--- data/examples/beta-Li3PS4/df_cfg.bash
+++ data/examples/beta-Li3PS4/df_cfg.bash
@@ -8,7 +8,7 @@ do
   pref=Li3PS4_Li2_${Li2p}
   mkdir ${pref}/
   cd ${pref}/
-  supercell -v 2 -i Li3PS4_PNMA_2011_icsd180319.cif -m -q -o ${pref} -p "Li2:p=${Li2p}" -p "Li3:p=${Li3p}" -s 1x1x1 > log.out &
+  supercell -v 2 -i ../Li3PS4_PNMA_2011_icsd180319.cif -m -q -o ${pref} -p "Li2:p=${Li2p}" -p "Li3:p=${Li3p}" -s 1x1x1 > log.out &
   cd ${cur_path}
 done
 
