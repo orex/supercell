e0060032e97a75e028170149ad91bec2762255d5
kirill <kirill.okhotnikov@gmail.com>
2016-07-06 01:25:22 +0300

    dpl

--- deploy.sh
+++ deploy.sh
@@ -46,6 +46,7 @@ git add --no-ignore-removal .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTION_LABEL="afb0f7bfe5ee"
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
