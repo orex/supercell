7b0a184ac6374a99b672a777ddbcdfa959c10aa7
kirill <kirill.okhotnikov@gmail.com>
2017-04-15 13:44:11 +0300

    Tutotial: support bib bibliography.

--- scripts/latex_compile.sh
+++ scripts/latex_compile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-pdflatex -interaction=nonstopmode -shell-escape  $1 
+pdflatex -interaction=nonstopmode -shell-escape  $1
+bibtex $1
+pdflatex -interaction=nonstopmode -shell-escape  $1
 pdflatex -interaction=nonstopmode -shell-escape  $1
 
 exit $?
